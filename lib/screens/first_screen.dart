ebb199a073514fc940b0d66ffdf8248c9667c9e2
Abid <abidroid1@gmail.com>
2024-12-28 11:50:57 +0500

    Initial commit

---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        title: Text('AKTI 4'),
+
+
+      ),
+      body: Text('My Calculator', style: TextStyle(
+        fontSize: 50.0,
+        color: Colors.blue,
+        fontStyle: FontStyle.italic,
+        fontWeight: FontWeight.bold,
+        wordSpacing: 15,
+   
+        backgroundColor: Colors.amber,
+      ),),
+    );
+  }
+}
